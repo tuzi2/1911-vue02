278ad4aef9288729ed24f4a996a2d9809a148322
ning <56795657@qq.com>
2020-08-16 19:20:26 +0800

    模板

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Index from '@/components/Index'
+import Category from '@/components/Category'
+import News from '@/components/News'
+import Search from '@/components/Search'
+import FullWidth from '@/components/FullWidth'
+import NotFound from '@/components/NotFound'
 
 Vue.use(Router)
 
@@ -8,8 +14,33 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Index',
+      component: Index
+    },
+    {
+      path: '/category',
+      name: 'Category',
+      component: Category
+    },
+    {
+      path: '/news',
+      name: 'News',
+      component: News
+    },
+    {
+      path: '/search',
+      name: 'Search',
+      component: Search
+    },
+    {
+      path: '/full-width',
+      name: 'FullWidth',
+      component: FullWidth
+    },
+    {
+      path: '/404',
+      name: 'NotFound',
+      component: NotFound
     }
   ]
 })
